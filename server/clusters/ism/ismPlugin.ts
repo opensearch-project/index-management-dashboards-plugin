76ec7d33057b6810f51c01927b2aa853c931f913
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-04-19 10:09:55 -0700

    Cleans up a few more references

--- server/clusters/ism/ismPlugin.ts
+++ server/clusters/ism/ismPlugin.ts
@@ -16,8 +16,9 @@
 import { API } from "../../utils/constants";
 
 /*
+    TODO: migrate to types
  * Types are not available until 7.2
- * https://github.com/elastic/kibana/blob/v7.2.0/src/core/server/elasticsearch/elasticsearch_client_config.ts
+ * https://github.com/opensearch-project/OpenSearch-Dashboards/blob/main/src/core/server/opensearch/client/client_config.ts
  * */
 export default function ismPlugin(Client: any, config: any, components: any) {
   const ca = components.clientAction.factory;
