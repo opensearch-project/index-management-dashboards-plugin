48798b4ccf1a5b71c1c7409722f9f33b6b511e03
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-08-10 09:43:27 -0700

    Adds custom label to use with EuiForm component
    Signed-off-by: Drew Baugher <46505179+dbbaughe@users.noreply.github.com>

---
+++ public/pages/VisualCreatePolicy/components/EuiFormCustomLabel/EuiFormCustomLabel.tsx
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+import React from "react";
+import { EuiText } from "@elastic/eui";
+
+interface EuiFormCustomLabelProps {
+  title: string;
+  helpText?: string;
+  textStyle?: object;
+  headerStyle?: object;
+}
+
+// New pattern for label and help text both being above the form row instead of label above and help below.
+const EuiFormCustomLabel = ({
+  title,
+  helpText,
+  textStyle = { marginBottom: "5px" },
+  headerStyle = { marginBottom: "2px" },
+}: EuiFormCustomLabelProps) => (
+  <EuiText style={textStyle}>
+    <h5 style={headerStyle}>{title}</h5>
+    <p>
+      {" "}
+      {/* Keep the <p> tag even if no helpText to remove last child styling on h tags */}
+      {helpText && <span style={{ color: "grey", fontWeight: 200, fontSize: "12px" }}>{helpText}</span>}
+    </p>
+  </EuiText>
+);
+
+export default EuiFormCustomLabel;
