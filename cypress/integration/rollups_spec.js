99c9bf3c64d21c4f7f62c1571c3dc2114f4c8ef3
Chris Hesterman <phestech@amazon.com>
2022-10-05 13:10:20 -0700

    Add 10s wait line19, rollups_spec to help find button next step
    Signed-off-by: Chris Hesterman <phestech@amazon.com>

--- cypress/integration/rollups_spec.js
+++ cypress/integration/rollups_spec.js
@@ -14,11 +14,12 @@ describe("Rollups", () => {
     localStorage.setItem("home:welcome:show", "true");
 
     // Go to sample data page
-    cy.visit(`${Cypress.env("opensearch_dashboards")}/app/home#/`);
+    cy.visit(`${Cypress.env("opensearch_dashboards")}/app/home#/tutorial_directory`);
 
+    cy.wait(10000)
 
-    // Click on "Sample data" tab
-    cy.contains("Add data").click({ force: true });
+    // // Click on "Sample data" tab
+    // cy.contains("Add data").click({ force: true });
 
     // Load sample eCommerce data
     cy.get(`button[data-test-subj="addSampleDataSetecommerce"]`).click({ force: true });
