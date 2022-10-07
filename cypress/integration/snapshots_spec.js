0cd6affec92aa4146601bd211ba078370942e37a
Chris Hesterman <phestech@amazon.com>
2022-10-07 08:59:27 -0700

    Remove vertical space, snapshots_spec
    Signed-off-by: Chris Hesterman <phestech@amazon.com>

--- cypress/integration/snapshots_spec.js
+++ cypress/integration/snapshots_spec.js
@@ -63,9 +63,6 @@ describe("Snapshots", () => {
       cy.get(`[data-test-subj="indicesComboBoxInput"]`).type("test_index_2{enter}");
       cy.get(`[data-test-subj="indicesComboBoxInput"]`).type("test_index_3{enter}");
 
-
-
-
       // Click 'Add' button to create snapshot
       cy.get("button").contains("Add").click({ force: true });
 
