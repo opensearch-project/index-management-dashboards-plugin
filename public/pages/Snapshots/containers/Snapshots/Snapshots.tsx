71bd1311cda89781b22e3de8da552d9e90897bfb
Chris Hesterman <phestech@amazon.com>
2022-10-18 09:24:20 -0700

    Update help text, Snapshots.jsx
    Signed-off-by: Chris Hesterman <phestech@amazon.com>

--- public/pages/Snapshots/containers/Snapshots/Snapshots.tsx
+++ public/pages/Snapshots/containers/Snapshots/Snapshots.tsx
@@ -418,7 +418,7 @@ export default class Snapshots extends Component<SnapshotsProps, SnapshotsState>
     const subTitleText = (
       <EuiText color="subdued" size="s" style={{ padding: "5px 0px" }}>
         <p style={{ fontWeight: 200 }}>
-          Snapshots are taken automatically from snapshot policies, <br />or you can initiate manual snapshots to save to a repository.<br />
+          Snapshots of indices are taken automatically from snapshot policies, <br />or you can initiate manual snapshots to save to a repository.<br />
           You can restore indices by selecting a snapshot.
         </p>
       </EuiText>
