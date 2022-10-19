0d17976fb588a7ceaba8794d32e420b9f37de7fa
Zhou Su <suzhou@amazon.com>
2022-10-19 17:34:18 +0800

    feat: modify some test spec
    Signed-off-by: Zhou Su <suzhou@amazon.com>

--- public/pages/CreateIndex/components/IndexMapping/IndexMapping.test.tsx
+++ public/pages/CreateIndex/components/IndexMapping/IndexMapping.test.tsx
@@ -9,7 +9,17 @@ import IndexMapping from "./IndexMapping";
 
 describe("<IndexMapping /> spec", () => {
   it("renders the component", () => {
-    const { container } = render(<IndexMapping />);
+    const { container } = render(<IndexMapping onChange={() => {}} />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("render mappings with object type", () => {
+    const { container } = render(
+      <IndexMapping
+        onChange={() => {}}
+        value={[{ fieldName: "object", type: "object", properties: [{ fieldName: "text", type: "text" }] }]}
+      />
+    );
+    expect(container).toMatchSnapshot();
   });
 });
