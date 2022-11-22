1fc68dd04e65f957d79d166af4a3efa5d22fe259
Hailong Cui <ihailong@amazon.com>
2022-11-22 13:58:28 +0800

    create index flyout
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- public/pages/Reindex/components/CreateIndexFlyout/CreateIndexFlyout.tsx
+++ public/pages/Reindex/components/CreateIndexFlyout/CreateIndexFlyout.tsx
@@ -6,16 +6,20 @@
 import React from "react";
 import CreateIndex, { IndexFormProps } from "../../../CreateIndex/containers/IndexForm";
 import { CoreServicesContext } from "../../../../components/core_services";
-import { EuiFlyout, EuiFlyoutBody, EuiFlyoutHeader, EuiTitle } from "@elastic/eui";
+import { EuiFlyout, EuiFlyoutBody, EuiFlyoutFooter, EuiFlyoutHeader, EuiTitle } from "@elastic/eui";
+import FlyoutFooter from "../../../VisualCreatePolicy/components/FlyoutFooter";
 
 interface CreateIndexFlyoutProps extends IndexFormProps {
   sourceIndices: string[];
+  onCloseFlyout: () => void;
 }
 
 export default class CreateIndexFlyout extends React.Component<CreateIndexFlyoutProps> {
   static contextType = CoreServicesContext;
 
   render() {
+    const { onCloseFlyout } = this.props;
+    const createIndexRef = React.createRef<CreateIndex>();
     return (
       <EuiFlyout onClose={() => {}} hideCloseButton>
         <EuiFlyoutHeader hasBorder>
@@ -25,8 +29,12 @@ export default class CreateIndexFlyout extends React.Component<CreateIndexFlyout
         </EuiFlyoutHeader>
 
         <EuiFlyoutBody>
-          <CreateIndex {...this.props}></CreateIndex>
+          <CreateIndex ref={createIndexRef} hideButtons={true} {...this.props} />
         </EuiFlyoutBody>
+
+        <EuiFlyoutFooter>
+          <FlyoutFooter action="" text="Create" edit={false} onClickCancel={onCloseFlyout} onClickAction={() => {}} />
+        </EuiFlyoutFooter>
       </EuiFlyout>
     );
   }
