31d7dd0d1b29912e7d75a2a8951f7b29d9e7d2f2
suzhou <suzhou@amazon.com>
2022-12-01 17:16:25 +0800

    feat: update unit test
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/pages/Indices/components/SplitIndexFlyout/index.ts
+++ public/pages/Indices/components/SplitIndexFlyout/index.ts
@@ -3,6 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import SplitIndexFlyout from "./SplitIndexFlyout.tsx";
+import SplitIndexFlyout from "./SplitIndexFlyout";
 
 export default SplitIndexFlyout;
