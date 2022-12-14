ef20b31e66892834b6c4a6badc935d1eb6b2eb45
suzhou <suzhou@amazon.com>
2022-12-14 16:33:25 +0800

    feat: update
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/pages/CreateIndex/components/AliasSelect/index.tsx
+++ public/pages/CreateIndex/components/AliasSelect/index.tsx
@@ -31,11 +31,20 @@ const AliasSelect = forwardRef((props: AliasSelectProps, ref: React.Ref<HTMLInpu
       if (res?.ok) {
         return {
           ...res,
-          response: [...new Set(res.response.filter((item) => !filterByMinimatch(item.alias, SYSTEM_ALIAS)))].map(
-            ({ alias, ...others }) => ({
-              ...others,
-              label: alias,
-            })
+          response: [...new Set(res.response.filter((item) => !filterByMinimatch(item.alias, SYSTEM_ALIAS)).map((item) => item.alias))].map(
+            (alias) => {
+              const findItem = res.response.find((item) => item.alias === alias);
+              if (findItem) {
+                return {
+                  ...findItem,
+                  label: findItem.alias,
+                };
+              }
+
+              return {
+                label: alias,
+              };
+            }
           ),
         };
       } else {
