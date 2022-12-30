88e95b2ad888ab383b3bf77544567b8ef567acab
suzhou <suzhou@amazon.com>
2022-12-30 16:59:06 +0800

    feat: update
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/pages/Rollover/containers/Rollover/Rollover.test.tsx
+++ public/pages/Rollover/containers/Rollover/Rollover.test.tsx
@@ -6,7 +6,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { MemoryRouter as Router } from "react-router";
 import { Route, RouteComponentProps, Switch } from "react-router-dom";
 import { browserServicesMock, coreServicesMock, apiCallerMock } from "../../../../../test/mocks";
@@ -23,7 +22,7 @@ function renderWithRouter(props: Omit<RolloverProps, keyof RouteComponentProps>,
           <ServicesContext.Provider value={browserServicesMock}>
             <ModalProvider>
               <Switch>
-                <Route path="/:index" render={(routeProps) => <Rollover {...props} {...routeProps} />} />
+                <Route path="/:source" render={(routeProps) => <Rollover {...props} {...routeProps} />} />
               </Switch>
             </ModalProvider>
           </ServicesContext.Provider>
@@ -38,53 +37,15 @@ describe("container <Rollover /> spec", () => {
     apiCallerMock(browserServicesMock);
   });
   it("render the component", async () => {
-    browserServicesMock.indexService.getIndices = jest.fn(() => {
+    browserServicesMock.indexService.getDataStreams = jest.fn(() => {
       return {
         ok: true,
-        response: {
-          indices: [
-            {
-              "docs.count": "5",
-              "docs.deleted": "2",
-              health: "green",
-              index: "test_index",
-              pri: "1",
-              "pri.store.size": "100KB",
-              rep: "0",
-              status: "open",
-              "store.size": "100KB",
-              uuid: "some_uuid",
-              managed: "",
-              managedPolicy: "",
-              data_stream: "",
-            },
-          ],
-        },
       } as any;
-    }) as typeof browserServicesMock.indexService.getIndices;
-    const { container, getByTestId, queryByText } = renderWithRouter({}, [`/test_index`]);
+    }) as typeof browserServicesMock.indexService.getDataStreams;
+    const { container } = renderWithRouter({}, [`/test_alias`]);
 
     await waitFor(() => {
       expect(container.firstChild).toMatchSnapshot();
-      expect(document.querySelector("#RolloverModalOverview")).not.toBeNull();
-      expect(browserServicesMock.commonService.apiCaller).toBeCalledTimes(1);
-      expect(browserServicesMock.commonService.apiCaller).toBeCalledWith({
-        endpoint: "indices.get",
-        data: {
-          index: "test_index",
-        },
-      });
-    });
-
-    userEvent.click(document.getElementById("RolloverModalAlias") as Element);
-    await waitFor(() => {
-      expect(queryByText("Index alias")).not.toBeNull();
-    });
-    userEvent.click(getByTestId("detailModalEdit"));
-    await waitFor(() => {});
-    userEvent.click(getByTestId("createIndexCreateButton"));
-    await waitFor(() => {
-      expect(browserServicesMock.commonService.apiCaller).toBeCalledTimes(4);
     });
   });
 });
