c5dce6729095862f5f7ec6e6b77bf1633f16b5e8
suzhou <suzhou@amazon.com>
2023-01-03 14:37:19 +0800

    Feature/index management 2.5 (#520)
    * Feature/common 2.5 (#506)
    
    * feat: split to common change
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * fix: detail of data stream
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    Signed-off-by: suzhou <suzhou@amazon.com>

---
+++ public/pages/CreateIndex/components/IndexDetail/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import IndexDetail from "./IndexDetail";
+
+export default IndexDetail;
+export * from "./IndexDetail";
