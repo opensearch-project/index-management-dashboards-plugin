7ede09b60b03ae66b0dadd3efc3683dd887277f3
suzhou <suzhou@amazon.com>
2023-02-13 12:13:50 +0800

    feat: modify validation message
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/utils/JSON_schemas/index_mappings/index.ts
+++ public/utils/JSON_schemas/index_mappings/index.ts
@@ -82,6 +82,7 @@ export const IndexMappingsJSONEditorSchema: JSONSchema4 = {
               },
               required: ["type", ...(item.options?.fields?.map((item) => item.name) || [])],
               additionalProperties: false,
+              errorMessage: "Property is not supported in the visual editor.",
             },
           };
 
