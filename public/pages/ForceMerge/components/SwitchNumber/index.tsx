a7a2ad9b721d3bba353dcfeb2618248456af7ebc
suzhou <suzhou@amazon.com>
2023-02-22 08:01:17 +0800

    Feature: force merge (#608)
    * temp: save
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add validator
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add unit test
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add unit test
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add cypress test
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: try to get cypress test start
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: force merge optimize
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: modify toast message
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update validation
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add debounce
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update validation
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update unit test
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: make unit test run
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: modify id
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: remove useless code
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update message
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: add readonly detect
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: protect
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: use the specific reason when force merge failed
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: suzhou <suzhou@amazon.com>
    
    ---------
    
    Signed-off-by: suzhou <suzhou@amazon.com>

---
+++ public/pages/ForceMerge/components/SwitchNumber/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { EuiRadioGroup, EuiSpacer } from "@elastic/eui";
+import CustomFormRow from "../../../../components/CustomFormRow";
+import { AllBuiltInComponents } from "../../../../components/FormGenerator";
+
+interface SwitchNumberProps {
+  value?: number;
+  onChange: (val: SwitchNumberProps["value"]) => void;
+}
+
+export default function SwitchNumber(props: SwitchNumberProps) {
+  const [id, setId] = useState(props.value && props.value > 0 ? "1" : "0");
+  return (
+    <>
+      <EuiRadioGroup
+        options={[
+          {
+            id: "0",
+            label: "Automatic",
+          },
+          {
+            id: "1",
+            label: "Manually set number of segments",
+          },
+        ]}
+        idSelected={id}
+        onChange={(id) => {
+          setId(id);
+          if (id === "0") {
+            props.onChange(undefined);
+          } else {
+            props.onChange(1);
+          }
+        }}
+      />
+      {id === "1" ? (
+        <>
+          <EuiSpacer size="s" />
+          <CustomFormRow
+            label="Number of segments"
+            helpText="Specify the number of segments to merge to. To fully merge indexes, set number to 1."
+          >
+            <AllBuiltInComponents.Number {...props} placeholder="Specify a number" />
+          </CustomFormRow>
+        </>
+      ) : null}
+    </>
+  );
+}
