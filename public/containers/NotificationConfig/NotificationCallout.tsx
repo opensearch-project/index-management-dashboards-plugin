295d79e0e1ed7c739761d85228cbd88c6443029f
suzhou <suzhou@amazon.com>
2023-04-24 15:53:10 +0800

    feat: add test case for NotificationCallout
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/containers/NotificationConfig/NotificationCallout.tsx
+++ public/containers/NotificationConfig/NotificationCallout.tsx
@@ -2,61 +2,69 @@ import { EuiCallOut, EuiLink, EuiSpacer } from "@elastic/eui";
 import React from "react";
 import { ROUTES } from "../../utils/constants";
 import { ActionType, OperationType, OperationTypeMapTitle } from "../../pages/Notifications/constant";
+import { getPermissionValue } from "./hooks";
 
-export default function NotificationCallout(props: {
+export interface INotificationCalloutProps {
   hasDefaultNotification: boolean;
   operationType?: OperationType;
   actionType: ActionType;
   permissionForViewLRON: boolean;
   permissionForCreateLRON: boolean;
-}) {
+}
+
+export default function NotificationCallout(props: INotificationCalloutProps) {
   const { hasDefaultNotification, operationType, actionType, permissionForCreateLRON, permissionForViewLRON } = props;
-  if (!hasDefaultNotification && permissionForViewLRON) {
+  const permissionValue = getPermissionValue(permissionForViewLRON, permissionForCreateLRON, hasDefaultNotification);
+
+  if (permissionValue === "110") {
     return null;
+  }
+
+  let title: React.ReactChild = <></>;
+  switch (permissionValue) {
+    case "000":
+      title = (
+        <>
+          Your administrator may have set default notification settings for {OperationTypeMapTitle[operationType || actionType]} operations.
+          Contact your administrator to request access to notification settings.
+        </>
+      );
+      break;
+    case "010":
+      title = (
+        <>
+          Your administrator may have set default notification settings for {OperationTypeMapTitle[operationType || actionType]} operations.
+          You can send additional notifications for this operation.
+        </>
+      );
+      break;
+    case "100":
+      title = (
+        <>
+          Your administrator has not set default notification settings for {OperationTypeMapTitle[operationType || actionType]} operations.
+        </>
+      );
+      break;
+    case "101":
+      title = (
+        <>Your administrator has set default notification settings for {OperationTypeMapTitle[operationType || actionType]} operations.</>
+      );
+    case "111":
+      title = (
+        <>
+          Your administrator has set default notification settings for {OperationTypeMapTitle[operationType || actionType]} operations.
+          Configure default settings at{" "}
+          <EuiLink style={{ textDecoration: "underline" }} href={`#${ROUTES.NOTIFICATIONS}`} target="_blank" external={false}>
+            Notifications
+          </EuiLink>
+          .
+        </>
+      );
   }
   return (
     <>
       <EuiSpacer size="s" />
-      <EuiCallOut
-        color="primary"
-        iconType="iInCircle"
-        title={
-          <>
-            {hasDefaultNotification ? (
-              <>
-                Your administrator has set default notification settings for {OperationTypeMapTitle[operationType || actionType]}{" "}
-                operations.{" "}
-                {permissionForCreateLRON ? (
-                  <>
-                    Configure default settings at{" "}
-                    <EuiLink style={{ textDecoration: "underline" }} href={`#${ROUTES.NOTIFICATIONS}`} target="_blank" external={false}>
-                      Notifications
-                    </EuiLink>
-                    .
-                  </>
-                ) : (
-                  <>Contact your administrator to request access to notification settings.</>
-                )}
-              </>
-            ) : permissionForCreateLRON ? (
-              <>
-                Your administrator may have set default notification settings for {OperationTypeMapTitle[operationType || actionType]}{" "}
-                operations. You can send additional notifications for this operation.
-              </>
-            ) : permissionForViewLRON ? (
-              <>
-                Your administrator has not set default notification settings for {OperationTypeMapTitle[operationType || actionType]}{" "}
-                operations.
-              </>
-            ) : (
-              <>
-                Your administrator may have set default notification settings for {OperationTypeMapTitle[operationType || actionType]}{" "}
-                operations. Contact your administrator to request access to notification settings.
-              </>
-            )}
-          </>
-        }
-      />
+      <EuiCallOut color="primary" iconType="iInCircle" data-test-subj="defaultNotificationCallout" title={title} />
     </>
   );
 }
