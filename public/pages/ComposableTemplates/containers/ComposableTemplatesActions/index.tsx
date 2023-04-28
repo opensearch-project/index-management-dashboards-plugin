2adb1b5e9d4c422b7148ab07520d4c646b2c9299
suzhou <suzhou@amazon.com>
2023-04-28 13:11:23 +0800

    feat: enable icons for actions
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/pages/ComposableTemplates/containers/ComposableTemplatesActions/index.tsx
+++ public/pages/ComposableTemplates/containers/ComposableTemplatesActions/index.tsx
@@ -11,17 +11,66 @@ import AssociatedTemplatesModal from "../AssociatedTemplatesModal";
 import { useComponentMapTemplate } from "../../utils/hooks";
 import { CoreServicesContext } from "../../../../components/core_services";
 
-export interface ComposableTemplatesActionsProps {
+export interface renderDeleteButtonProps {
   selectedItems: string[];
-  onDelete: () => void;
-  history: RouteComponentProps["history"];
+  triggerDelete: () => void;
 }
 
-export default function ComposableTemplatesActions(props: ComposableTemplatesActionsProps) {
+export interface ComposableTemplatesActionsProps {
+  selectedItems: renderDeleteButtonProps["selectedItems"];
+  onDelete: () => void;
+  history: RouteComponentProps["history"];
+  renderDeleteButton?: (props: renderDeleteButtonProps) => React.ReactChild;
+}
+
+export function ComposableTemplatesDeleteAction(props: ComposableTemplatesActionsProps) {
   const { selectedItems, onDelete } = props;
   const [deleteIndexModalVisible, setDeleteIndexModalVisible] = useState(false);
   const coreServices = useContext(CoreServicesContext) as CoreStart;
   const { componentMapTemplate } = useComponentMapTemplate();
+  const renderDeleteButton: (props: {
+    selectedItems: renderDeleteButtonProps["selectedItems"];
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  }) => React.ReactChild = (renderProps) => {
+    const triggerDelete = () => {
+      if (componentMapTemplate[selectedItems[0]]?.length) {
+        const toast = coreServices.notifications.toasts.addDanger({
+          title: `Unable to delete ${selectedItems[0]}`,
+          text: ((
+            <>
+              The component cannot be deleted when it is associated with {componentMapTemplate[selectedItems[0]]?.length} templates. Unlink
+              the component from all templates and try again.
+              <EuiSpacer />
+              <EuiButton
+                color="danger"
+                data-test-subj="viewAssociatedTemplatesInToast"
+                onClick={() => {
+                  coreServices.notifications.toasts.remove(toast.id);
+                  renderProps.setVisible(true);
+                }}
+              >
+                View associated index templates
+              </EuiButton>
+            </>
+          ) as unknown) as string,
+        });
+      } else {
+        setDeleteIndexModalVisible(true);
+      }
+    };
+    if (props.renderDeleteButton) {
+      return props.renderDeleteButton({
+        ...renderProps,
+        triggerDelete,
+      });
+    }
+
+    return (
+      <EuiButton data-test-subj="deleteAction" color="danger" disabled={renderProps.selectedItems.length !== 1} onClick={triggerDelete}>
+        Delete
+      </EuiButton>
+    );
+  };
 
   const onDeleteIndexModalClose = () => {
     setDeleteIndexModalVisible(false);
@@ -31,41 +80,7 @@ export default function ComposableTemplatesActions(props: ComposableTemplatesAct
     <>
       <AssociatedTemplatesModal
         componentTemplate={selectedItems[0]}
-        renderProps={({ setVisible }) => (
-          <EuiButton
-            data-test-subj="deleteAction"
-            color="danger"
-            disabled={selectedItems.length !== 1}
-            onClick={() => {
-              if (componentMapTemplate[selectedItems[0]]?.length) {
-                const toast = coreServices.notifications.toasts.addDanger({
-                  title: `Unable to delete ${selectedItems[0]}`,
-                  text: ((
-                    <>
-                      The component cannot be deleted when it is associated with {componentMapTemplate[selectedItems[0]]?.length} templates.
-                      Unlink the component from all templates and try again.
-                      <EuiSpacer />
-                      <EuiButton
-                        color="danger"
-                        data-test-subj="viewAssociatedTemplatesInToast"
-                        onClick={() => {
-                          coreServices.notifications.toasts.remove(toast.id);
-                          setVisible(true);
-                        }}
-                      >
-                        View associated index templates
-                      </EuiButton>
-                    </>
-                  ) as unknown) as string,
-                });
-              } else {
-                setDeleteIndexModalVisible(true);
-              }
-            }}
-          >
-            Delete
-          </EuiButton>
-        )}
+        renderProps={({ setVisible }) => renderDeleteButton({ selectedItems, setVisible })}
       />
       <DeleteIndexModal
         selectedItems={selectedItems}
@@ -78,4 +93,8 @@ export default function ComposableTemplatesActions(props: ComposableTemplatesAct
       />
     </>
   );
+}
+
+export default function ComposableTemplatesAction(props: ComposableTemplatesActionsProps) {
+  return <ComposableTemplatesDeleteAction {...props} />;
 }
