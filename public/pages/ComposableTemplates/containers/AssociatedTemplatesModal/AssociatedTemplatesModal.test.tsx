e9a67f8c23b7751524774611e0ffddacf735c440
suzhou <suzhou@amazon.com>
2023-05-10 13:49:23 +0800

    feat: optimize unit test
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/pages/ComposableTemplates/containers/AssociatedTemplatesModal/AssociatedTemplatesModal.test.tsx
+++ public/pages/ComposableTemplates/containers/AssociatedTemplatesModal/AssociatedTemplatesModal.test.tsx
@@ -5,24 +5,48 @@
 
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import AssociatedTemplatesModal from "./AssociatedTemplatesModal";
+import userEvent from "@testing-library/user-event";
 import { browserServicesMock, coreServicesMock } from "../../../../../test/mocks";
 import { ServicesContext } from "../../../../services";
 import { CoreServicesContext } from "../../../../components/core_services";
 
 describe("<AssociatedTemplatesModal /> spec", () => {
   it("renders the component", async () => {
-    render(
+    browserServicesMock.commonService.apiCaller = jest.fn(
+      async (payload): Promise<any> => {
+        if (payload.endpoint === "transport.request" && payload.data.path === "_index_template/*") {
+          return {
+            ok: true,
+            response: {
+              index_templates: [],
+            },
+          };
+        }
+
+        return {
+          ok: true,
+        };
+      }
+    );
+    const { findByText, getByTestId } = render(
       <CoreServicesContext.Provider value={coreServicesMock}>
         <ServicesContext.Provider value={browserServicesMock}>
-          <AssociatedTemplatesModal componentTemplate="test" renderProps={() => <div>123</div>} />
+          <AssociatedTemplatesModal
+            componentTemplate="test"
+            renderProps={({ setVisible }) => (
+              <div data-test-subj="test" onClick={() => setVisible(true)}>
+                123
+              </div>
+            )}
+          />
         </ServicesContext.Provider>
       </CoreServicesContext.Provider>
     );
-    await waitFor(() => {}, {
-      timeout: 3000,
-    });
+    await findByText("123");
+    await userEvent.click(getByTestId("test"));
+    await findByText("Associated index templates");
     expect(document.body.children).toMatchSnapshot();
   });
 });
