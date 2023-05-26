213a2f9f2f70527391353b0a3cc8625d96fdb82e
suzhou <suzhou@amazon.com>
2023-05-26 14:53:55 +0800

    feat: hide invalid channel
    Signed-off-by: suzhou <suzhou@amazon.com>

--- public/containers/ChannelSelect/ChannelSelect.tsx
+++ public/containers/ChannelSelect/ChannelSelect.tsx
@@ -7,6 +7,7 @@ import React from "react";
 import { EuiComboBoxOptionOption, EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 import { useChannels } from "./hooks";
 import { AllBuiltInComponents } from "../../components/FormGenerator";
+import "./index.scss";
 
 export interface ChannelSelectProps {
   value?: { id: string }[];
@@ -21,9 +22,10 @@ const ChannelSelect = (props: ChannelSelectProps) => {
     <EuiFlexGroup data-test-subj={props["data-test-subj"]} gutterSize="s" style={{ maxWidth: "unset", minWidth: 300 }}>
       <EuiFlexItem>
         <AllBuiltInComponents.ComboBoxMultiple
+          className="channelSelect-combobox"
           placeholder="Select channel"
           isLoading={loading}
-          options={channels.map((channel) => ({ value: channel.config_id, label: channel.name }))}
+          options={channels.map((channel) => ({ value: channel.config_id, label: channel.name, className: "valid-option" }))}
           onChange={(val, options: EuiComboBoxOptionOption<string>[]) => {
             onChange(
               options.map((item) => ({
