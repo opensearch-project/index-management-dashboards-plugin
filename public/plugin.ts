f54155d45e60c338cdee4cd848fb1bdb6e94b730
Hailong Cui <ihailong@amazon.com>
2023-07-11 10:19:02 +0800

    Move index management from plugin section to management section (#741) (#805)
    * Move index management into management section
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * Remove plugins pages for management overview registration
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * wording changes
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    ---------
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    (cherry picked from commit 4352f54c564d02d36ce6b7b412a9875d6254ddb8)

--- public/plugin.ts
+++ public/plugin.ts
@@ -3,29 +3,58 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AppMountParameters, CoreSetup, CoreStart, Plugin, PluginInitializerContext } from "../../../src/core/public";
-import { IndexManagementPluginSetup } from ".";
-import { IndexManagementPluginStart } from ".";
+import { i18n } from "@osd/i18n";
+import { IndexManagementPluginStart, IndexManagementPluginSetup } from ".";
+import {
+  AppMountParameters,
+  CoreSetup,
+  CoreStart,
+  DEFAULT_APP_CATEGORIES,
+  Plugin,
+  PluginInitializerContext,
+} from "../../../src/core/public";
 import { actionRepoSingleton } from "./pages/VisualCreatePolicy/utils/helpers";
 import { ROUTES } from "./utils/constants";
 import { JobHandlerRegister } from "./JobHandler";
+import { ManagementOverViewPluginSetup } from "../../../src/plugins/management_overview/public";
 
-export class IndexManagementPlugin implements Plugin<IndexManagementPluginSetup, IndexManagementPluginStart> {
+interface IndexManagementSetupDeps {
+  managementOverview?: ManagementOverViewPluginSetup;
+}
+
+export class IndexManagementPlugin implements Plugin<IndexManagementPluginSetup, IndexManagementPluginStart, IndexManagementSetupDeps> {
   constructor(private readonly initializerContext: PluginInitializerContext) {
     // can retrieve config from initializerContext
   }
 
-  public setup(core: CoreSetup): IndexManagementPluginSetup {
+  public setup(core: CoreSetup, { managementOverview }: IndexManagementSetupDeps): IndexManagementPluginSetup {
     JobHandlerRegister(core);
+
+    if (managementOverview) {
+      managementOverview.register({
+        id: "opensearch_index_management_dashboards",
+        title: "Index Management",
+        order: 9010,
+        description: i18n.translate("indexManagement.description", {
+          defaultMessage: "Manage your indexes with state polices, templates and aliases. You can also roll up or transform your indexes.",
+        }),
+      });
+      managementOverview.register({
+        id: "opensearch_snapshot_management_dashboards",
+        title: "Snapshot Management",
+        order: 9020,
+        description: i18n.translate("snapshotManagement.description", {
+          defaultMessage:
+            "Back up and restore your cluster's indexes and state. Setup a policy to automate snapshot creation and deletion.",
+        }),
+      });
+    }
+
     core.application.register({
       id: "opensearch_index_management_dashboards",
       title: "Index Management",
-      order: 7000,
-      category: {
-        id: "opensearch",
-        label: "OpenSearch Plugins",
-        order: 2000,
-      },
+      order: 9010,
+      category: DEFAULT_APP_CATEGORIES.management,
       mount: async (params: AppMountParameters) => {
         const { renderApp } = await import("./index_management_app");
         const [coreStart, depsStart] = await core.getStartServices();
@@ -36,12 +65,8 @@ export class IndexManagementPlugin implements Plugin<IndexManagementPluginSetup,
     core.application.register({
       id: "opensearch_snapshot_management_dashboards",
       title: "Snapshot Management",
-      order: 7000,
-      category: {
-        id: "opensearch",
-        label: "OpenSearch Plugins",
-        order: 2000,
-      },
+      order: 9020,
+      category: DEFAULT_APP_CATEGORIES.management,
       mount: async (params: AppMountParameters) => {
         const { renderApp } = await import("./index_management_app");
         const [coreStart, depsStart] = await core.getStartServices();
