1120c120c938084de9da4f4f5ed40497e579db5c
kohinoor98 <kohinoorchatterjee1998@gmail.com>
2023-11-15 01:14:13 -0800

    reverting tsconfig, precommit and github action
    Signed-off-by: kohinoor98 <kohinoorchatterjee1998@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 const LICENSE_HEADER = `
 /*
  * Copyright OpenSearch Contributors
@@ -10,28 +15,6 @@ module.exports = {
   extends: ["@elastic/eslint-config-kibana", "plugin:@elastic/eui/recommended"],
   rules: {
     // "@osd/eslint/require-license-header": "off"
-    "import/no-default-export": "off",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "default",
-        format: ["camelCase", "UPPER_CASE", "PascalCase", "snake_case"],
-        leadingUnderscore: "allow",
-        trailingUnderscore: "allow",
-      },
-    ],
-    "@osd/eslint/no-restricted-paths": [
-      "error",
-      {
-        basePath: __dirname,
-        zones: [
-          {
-            target: ["(public|server)/**/*"],
-            from: ["../../packages/**/*", "packages/**/*"],
-          },
-        ],
-      },
-    ],
   },
   overrides: [
     {
