4cdae27c328d95716cb770210bf764215b470e4d
Rohit Ashiwal <rashiwal@amazon.com>
2024-03-26 17:26:35 +0530

    cypress: modify test to check for inequality
    Signed-off-by: Rohit Ashiwal <rashiwal@amazon.com>

--- cypress/integration/plugins/index-management-dashboards-plugin/aliases.js
+++ cypress/integration/plugins/index-management-dashboards-plugin/aliases.js
@@ -167,7 +167,7 @@ describe("Aliases", () => {
       }).then((response) => {
         let response_obj = JSON.parse(response["allRequestResponses"][0]["Response Body"]);
         let num = response_obj["_all"]["total"]["translog"]["uncommitted_operations"];
-        expect(num).to.equal(1);
+        expect(num).not.equal(0);
       });
 
       cy.get('[data-test-subj="moreAction"]').click();
