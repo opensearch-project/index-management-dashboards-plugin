830d542e3bfe162025c4a0b2c64423bb8259e7aa
Vaibhav Agarwal <vaibhoag@amazon.com>
2025-04-02 17:45:02 +0530

    fixing run-test binary installation
    Signed-off-by: Vaibhav Agarwal <vaibhoag@amazon.com>

--- babel.config.js
+++ babel.config.js
@@ -6,10 +6,23 @@
 // babelrc doesn't respect NODE_PATH anymore but using require does.
 // Alternative to install them locally in node_modules
 module.exports = function (api) {
-  // ensure env is test so that this config won't impact build or dev server
   if (api.env("test")) {
     return {
-      presets: [require("@babel/preset-env"), require("@babel/preset-react"), require("@babel/preset-typescript")],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              node: "current",
+            },
+            features: {
+              "proposal-nullish-coalescing-operator": true,
+            },
+          },
+        ],
+        require("@babel/preset-react"),
+        require("@babel/preset-typescript"),
+      ],
       plugins: [
         [require("@babel/plugin-transform-runtime"), { regenerator: true }],
         require("@babel/plugin-transform-class-properties"),
