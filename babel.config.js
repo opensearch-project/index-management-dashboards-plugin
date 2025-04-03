e7dfc1433fe771f0fb3a767d0570777f959b832d
Vaibhav Agarwal <vaibhoag@amazon.com>
2025-04-04 00:28:10 +0530

    Fixed monaco dependency
    Signed-off-by: Vaibhav Agarwal <vaibhoag@amazon.com>

--- babel.config.js
+++ babel.config.js
@@ -6,8 +6,13 @@
 // babelrc doesn't respect NODE_PATH anymore but using require does.
 // Alternative to install them locally in node_modules
 module.exports = function (api) {
-  // ensure env is test so that this config won't impact build or dev server
-  if (api.env("test")) {
+  const isTest = api.env("test");
+
+  // Common plugins for all environments
+  const commonPlugins = [require("@babel/plugin-proposal-nullish-coalescing-operator")];
+
+  // Test-specific configuration
+  if (isTest) {
     return {
       presets: [require("@babel/preset-env"), require("@babel/preset-react"), require("@babel/preset-typescript")],
       plugins: [
@@ -15,9 +20,13 @@ module.exports = function (api) {
         require("@babel/plugin-transform-class-properties"),
         require("@babel/plugin-transform-object-rest-spread"),
         [require("@babel/plugin-transform-modules-commonjs"), { allowTopLevelThis: true }],
-        require("@babel/plugin-proposal-nullish-coalescing-operator"),
+        ...commonPlugins,
       ],
     };
   }
-  return {};
+
+  // Build/dev configuration
+  return {
+    plugins: commonPlugins,
+  };
 };
--- public/lib/monaco/json.ts
+++ public/lib/monaco/json.ts
@@ -1,56 +1,68 @@
 import { monaco } from "@osd/monaco";
-// @ts-ignore
-import * as mode from "monaco-editor/esm/vs/language/json/jsonMode.js"; // eslint-disable-line
 
-const Emitter = monaco.Emitter;
-class LanguageServiceDefaultsImpl {
-  _onDidChange: monaco.Emitter<any>;
-  _languageId: string;
-  _diagnosticsOptions!: monaco.languages.json.DiagnosticsOptions;
-  constructor(languageId: string, diagnosticsOptions: monaco.languages.json.DiagnosticsOptions) {
-    this._onDidChange = new Emitter();
-    this._languageId = languageId;
-    this.setDiagnosticsOptions(diagnosticsOptions);
-  }
-  public get languageId() {
-    return this._languageId;
-  }
-  public get onDidChange() {
-    return this._onDidChange.event;
-  }
-  public get diagnosticsOptions() {
-    return this._diagnosticsOptions;
-  }
-  setDiagnosticsOptions(options: monaco.languages.json.DiagnosticsOptions) {
-    this._diagnosticsOptions = options || Object.create(null);
-    this._onDidChange.fire(this);
-  }
-}
-export { LanguageServiceDefaultsImpl };
-const diagnosticDefault = {
-  validate: true,
-  allowComments: true,
-  schemas: [],
-  enableSchemaRequest: false,
-};
-/**
- * In case there are other plugins register the language implementation.
- */
-if (!monaco.languages.json) {
-  const jsonDefaults = new LanguageServiceDefaultsImpl("json", diagnosticDefault);
-  function createAPI() {
-    return {
-      jsonDefaults,
-    };
-  }
-  monaco.languages.json = createAPI();
+// Use the existing Monaco JSON language features
+// This ensures we're using the version that's already configured in OpenSearch Dashboards
+
+// Make sure JSON language is registered
+if (!monaco.languages.getLanguages().some((lang) => lang.id === "json")) {
   monaco.languages.register({
     id: "json",
     extensions: [".json", ".bowerrc", ".jshintrc", ".jscsrc", ".eslintrc", ".babelrc"],
     aliases: ["JSON", "json"],
     mimetypes: ["application/json"],
   });
-  monaco.languages.onLanguage("json", function () {
-    mode.setupMode(jsonDefaults);
-  });
+}
+
+// Set default diagnostics options if json language is available
+if (monaco.languages.json) {
+  const diagnosticDefault = {
+    validate: true,
+    allowComments: true,
+    schemas: [],
+    enableSchemaRequest: false,
+  };
+
+  // Only set diagnostics options if the method exists
+  if (monaco.languages.json.jsonDefaults && typeof monaco.languages.json.jsonDefaults.setDiagnosticsOptions === "function") {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions(diagnosticDefault);
+  }
+}
+
+// Export a compatible interface for backward compatibility
+export class LanguageServiceDefaultsImpl {
+  _onDidChange: monaco.Emitter<any>;
+  _languageId: string;
+  _diagnosticsOptions!: monaco.languages.json.DiagnosticsOptions;
+
+  constructor(languageId: string, diagnosticsOptions: monaco.languages.json.DiagnosticsOptions) {
+    this._onDidChange = new monaco.Emitter();
+    this._languageId = languageId;
+    this.setDiagnosticsOptions(diagnosticsOptions);
+  }
+
+  public get languageId() {
+    return this._languageId;
+  }
+
+  public get onDidChange() {
+    return this._onDidChange.event;
+  }
+
+  public get diagnosticsOptions() {
+    return this._diagnosticsOptions;
+  }
+
+  setDiagnosticsOptions(options: monaco.languages.json.DiagnosticsOptions) {
+    this._diagnosticsOptions = options || Object.create(null);
+    this._onDidChange.fire(this);
+
+    // If monaco.languages.json exists, also update its diagnostics options
+    if (
+      monaco.languages.json &&
+      monaco.languages.json.jsonDefaults &&
+      typeof monaco.languages.json.jsonDefaults.setDiagnosticsOptions === "function"
+    ) {
+      monaco.languages.json.jsonDefaults.setDiagnosticsOptions(options);
+    }
+  }
 }
