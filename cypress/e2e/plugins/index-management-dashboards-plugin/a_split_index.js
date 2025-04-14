53b777cd7cf1da028ceb92ec912b8994ecf63455
Vaibhav Agarwal <vaibhoag@amazon.com>
2025-04-14 17:08:58 +0530

    debug split_index
    Signed-off-by: Vaibhav Agarwal <vaibhoag@amazon.com>

--- cypress/e2e/plugins/index-management-dashboards-plugin/a_split_index.js
+++ cypress/e2e/plugins/index-management-dashboards-plugin/a_split_index.js
@@ -39,17 +39,17 @@ describe("Split Index", () => {
       // enter create page
       cy.get('[data-test-subj="Create IndexButton"]').click();
       cy.contains("Create index");
-      //
-      // // type field name
-      // cy.get('[placeholder="Specify a name for the new index."]').type(sampleIndex).end();
-      //
-      // cy.get('[data-test-subj="comboBoxSearchInput"]').focus().type(`${sampleAlias}{enter}`).end();
-      //
-      // // click create
-      // cy.get('[data-test-subj="createIndexCreateButton"]').click({ force: true }).end();
-      //
-      // // The index should exist
-      // cy.get(`#_selection_column_${sampleIndex}-checkbox`).should("have.exist").end();
+
+      // type field name
+      cy.get('[placeholder="Specify a name for the new index."]').type(sampleIndex).end();
+
+      cy.get('[data-test-subj="comboBoxSearchInput"]').focus().type(`${sampleAlias}{enter}`).end();
+
+      // click create
+      cy.get('[data-test-subj="createIndexCreateButton"]').click({ force: true }).end();
+
+      // The index should exist
+      cy.get(`#_selection_column_${sampleIndex}-checkbox`).should("have.exist").end();
     });
   });
 });
